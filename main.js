6f557656510fa3a567335dab585473af6f7b913f
Nikolaev Nikita <69678682+NikitkaN@users.noreply.github.com>
2022-06-07 12:15:52 +0300

    Final

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
+const begin = document.querySelector('.begin');
 const container = document.querySelector('.container');
+const footer = document.querySelector('footer');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 
 slider.style.display = 'none';
 
 function PlayClick() {
     var audio = new Audio('Y2mate.mx - Vigilante Crusade (192 kbps).mp3');
-    audio.play();
+    // audio.play();
     audio.loop = true;
     audio.volume = 0.5;
 
-    container.style.display = 'none';
-    slider.style.display = 'grid';
+    begin.classList.add('fade');
+    setTimeout(() => begin.style.display = 'none', 2000);
+    // left.classList.add('antifade');
+    // right.classList.add('antifade');
+    container.style.display = 'grid';
 }
 
 document.addEventListener('keydown', function(event) {
-    if (event.code == 'ArrowRight') {
+    if (event.code == 38) {
         showSlides(slideIndex += 1);
        
     }
-    if (event.code == 'ArrowLeft') {
+    if (event.code == 40) {
         showSlides(slideIndex -= 1);
     }
 });
